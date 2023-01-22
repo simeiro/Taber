5fd01cc65089c500c59487420d5e127f8ddfcc4c
fecia <96968513+fecia@users.noreply.github.com>
2023-01-22 18:44:38 +0900

    タブ検索機能完成
    今のところはpopupした時に毎回リセットされるからリセットされないようにしても良さそう。

--- main/popup.js
+++ main/popup.js
@@ -17,6 +17,10 @@ chrome.tabs.query({windowId : chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
 
 //Event
 window.addEventListener("load",() => {
+
+  const searchResult = document.getElementById("resultSelect");
+  const searchinput = document.getElementById("inputSearch");
+
   //copyボタンが押されたらtextareaの内容をクリップボードにコピーする
   document.querySelector("#copyButton").addEventListener("click", () => {
     document.querySelector("#txt").select();
@@ -35,18 +39,35 @@ window.addEventListener("load",() => {
       chrome.storage.local.set({maxTabNum : maxTabNum});
     });
   });
-
-//検索窓
-  document.getElementById("inputSearch").addEventListener("change", (event) =>{
+  
+  //検索窓 --shita
+  searchinput.addEventListener("change", (event) =>{
     console.log(event.target.value);
+    searchResult.replaceChildren();
     chrome.tabs.query({windowId : chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
       var input = event.target.value;
       tabs.forEach((tab) => {
-        if(tab.indexOf(input) > -1){
-          // 部分一致のときの処理
-        }
+        if(tab.title.toLowerCase().indexOf(input) > -1){
+          let option = document.createElement("option");
+          option.value = tab.id;
+          option.text = tab.title;
+          console.log(option)
+          searchResult.appendChild(option);
+        };
       });
-    })
-  })
+      let countTabs = document.createElement("option");
+      countTabs.setAttribute("selected", true);
+      countTabs.text = "ヒット件数: "+searchResult.childElementCount;
+      searchResult.prepend(countTabs);
+    });
+  });
+  //検索結果欄  --shita
+  //--検索結果欄初期表示  あとで検索結果のこしておく処理作りたい
+  const defaultOption = document.createElement("option");
+  defaultOption.text = "検索結果がここに表示されます。";
+  searchResult.replaceChildren(defaultOption);
+  //--
+  searchResult.addEventListener("change", (event) =>{
+    chrome.tabs.update(Number(event.target.value),{active : true});
+  });
 });
-
