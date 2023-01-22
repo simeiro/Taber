4a804d5c111654ec28a4e2f6df2e24842f86cac6
simeiro <shitimi1633@gmail.com>
2023-01-22 22:33:17 +0900

    アイコン作ったわ
    現在Tab数表示を関数化、また表示MAXの追加。タブ数によってアイコンが白から赤へと変化する機能を追加。

--- main/background.js
+++ main/background.js
@@ -1,34 +1,56 @@
 chrome.tabs.query({windowId : chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    //アイコンの下に現在のタブ数を表示
-    chrome.action.setBadgeText({text :  String(tabs.length)});
     //開けるタブの最大値の初期値を拡張機能更新時のタブ数にする
     chrome.storage.local.get("maxTabNum", (items) => {
         if(typeof items.maxNum == "undefined") {
             chrome.storage.local.set({maxTabNum : tabs.length});
         }
+        displayNum(tabs.length, items.maxTabNum);
+        makeIcon(tabs.length, items.maxTabNum);
     });
 });
 
 //タブ更新時に実行
 chrome.tabs.onUpdated.addListener((tabId) => {
     chrome.tabs.query({windowId : chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-        //アイコンの下に現在のタブ数を表示
-        chrome.action.setBadgeText({text :  String(tabs.length)});
         //ストレージに格納されているmaxTabNumよりタブ数が多ければ新しいタブを閉じる
         chrome.storage.local.get("maxTabNum", (items) => {
             if(tabs.length > items.maxTabNum){
                 chrome.tabs.remove(tabId);
             }
+            displayNum(tabs.length, items.maxTabNum);
+            makeIcon(tabs.length, items.maxTabNum);
         });
     });
 });
 
 //タブ削除時に実行
 chrome.tabs.onRemoved.addListener(() => {
-    //アイコンの下に現在のタブ数を表示
     chrome.tabs.query({windowId : chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-        chrome.action.setBadgeText({text : String(tabs.length)});
+        chrome.storage.local.get("maxTabNum", (items) => {
+            displayNum(tabs.length, items.maxTabNum);
+            makeIcon(tabs.length, items.maxTabNum);
+        });
     });
 });
 
-    
+
+//アイコンを作成する関数
+function makeIcon(tabsLength, maxTabNum){
+    const canvas = new OffscreenCanvas(16, 16);
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, 16, 16);
+    context.fillStyle = `rgb(255, ${255 - 255 * (tabsLength/maxTabNum)}, ${255- 255 * (tabsLength/maxTabNum)})`; //白→赤のグラデーション
+    context.fillRect(0, 0, 16, 16);
+    const imageData = context.getImageData(0, 0, 16, 16);
+    chrome.action.setIcon({imageData: imageData}, () => {
+        });
+}
+
+//アイコン下に現在のタブ数を表示する
+function displayNum(tabsLength, maxTabNum){
+    if(tabsLength == maxTabNum){
+        chrome.action.setBadgeText({text: String("MAX")});
+    }else{
+        chrome.action.setBadgeText({text :  String(tabsLength + '/' + maxTabNum)});
+    }
+}
