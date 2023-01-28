f6928d51389116181789ba08dbe6a6e535a113b2
fecia <96968513+fecia@users.noreply.github.com>
2023-01-28 21:05:21 +0900

    検索機能&オプション設定可能

--- main/popup.js
+++ main/popup.js
@@ -191,22 +191,52 @@ window.addEventListener("load", () => {
 
     //検索窓 --shita
     searchinput.addEventListener("change", (event) => {
-        searchResult.disabled = false;
-        searchResult.replaceChildren();
         chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-            var input = event.target.value;
-            tabs.forEach((tab) => {
-                if (tab.title.toLowerCase().indexOf(input) > -1) {
-                    let option = document.createElement("option");
-                    option.value = tab.id;
-                    option.text = tab.title;
-                    searchResult.appendChild(option);
-                };
+            searchResult.disabled = false;
+            searchResult.replaceChildren();
+            chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
+                chrome.storage.local.get(["tsm"],(value) => {
+                    var input = event.target.value;
+                    switch (value.tsm){
+                        case "0":
+                            tabs.forEach((tab) => {
+                                if (tab.title.toLowerCase().indexOf(input) > -1) {
+                                    let option = document.createElement("option");
+                                    option.value = tab.id;
+                                    option.text = tab.title;
+                                    searchResult.appendChild(option);
+                                };
+                            });
+                            break;
+
+                        case "1":
+                            tabs.forEach((tab) => {
+                                if (tab.url.toLowerCase().indexOf(input) > -1) {
+                                    let option = document.createElement("option");
+                                    option.value = tab.id;
+                                    option.text = tab.title;
+                                    searchResult.appendChild(option);
+                                };
+                            });
+                            break;
+
+                        case "2":
+                            tabs.forEach((tab) => {
+                                if (tab.title.toLowerCase().indexOf(input) > -1 || tab.url.toLowerCase().indexOf(input) > -1) {
+                                    let option = document.createElement("option");
+                                    option.value = tab.id;
+                                    option.text = tab.title;
+                                    searchResult.appendChild(option);
+                                };
+                            });
+                            break;
+                    };
+                    let countTabs = document.createElement("option");
+                    countTabs.setAttribute("selected", true);
+                    countTabs.text = "ヒット件数: " + searchResult.childElementCount;
+                    searchResult.prepend(countTabs);
+                });
             });
-            let countTabs = document.createElement("option");
-            countTabs.setAttribute("selected", true);
-            countTabs.text = "ヒット件数: " + searchResult.childElementCount;
-            searchResult.prepend(countTabs);
         });
     });
 
@@ -223,18 +253,37 @@ window.addEventListener("load", () => {
 
     //同一タブ削除ボタン --shita
     chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-        const onlyTabs = tabs.filter((tab, index, array) => {
-            return (array.findIndex(nextTab => tab.url === nextTab.url) !== index)
+        chrome.storage.local.get(["stm"],(value) => {
+            switch (value.stm){
+                case "0":
+                    var onlyTabs = tabs.filter((tab, index, array) => {
+                        console.log("0");
+                        return (array.findIndex(nextTab => tab.title === nextTab.title) !== index)});
+                    break;
+
+                case "1":
+                    var onlyTabs = tabs.filter((tab, index, array) => {
+                        console.log("1");
+                        return (array.findIndex(nextTab => tab.url === nextTab.url) !== index)});
+                    break;
+
+                case "2":
+                    var onlyTabs = tabs.filter((tab, index, array) => {
+                        console.log("2");
+                        // if(1 ===(array.findIndex(nextTab => tab.title === nextTab.title) !== index));
+                        return (array.findIndex(nextTab => tab.title === nextTab.title || tab.url === nextTab.url) !== index)});
+                    break;
+            };
+            if (onlyTabs.length != 0) {
+                const h_sametab = document.getElementById("sameTabDelete");
+                h_sametab.innerHTML = "重複タブ:" + Number(onlyTabs.length) + "個" + '<button class = "deletetabs_button" id="deleteTabsButton" disabled>削除</button>';
+                const deleteTab = document.getElementById("deleteTabsButton");
+                deleteTab.disabled = false;
+                deleteTab.addEventListener("click", (event) => {
+                    chrome.tabs.remove(onlyTabs.map((tab) => Number(tab.id)));
+                    h_sametab.textContent = "重複タブを削除しました。";
+                });
+            };
         });
-        if (onlyTabs.length != 0) {
-            const h_sametab = document.getElementById("sameTabDelete");
-            h_sametab.innerHTML = "重複タブ:" + Number(onlyTabs.length) + "個" + '<button class = "deletetabs_button" id="deleteTabsButton" disabled>削除</button>';
-            const deleteTab = document.getElementById("deleteTabsButton");
-            deleteTab.disabled = false;
-            deleteTab.addEventListener("click", (event) => {
-                chrome.tabs.remove(onlyTabs.map((tab) => Number(tab.id)));
-                h_sametab.textContent = "重複タブを削除しました。";
-            });
-        };
     });
 });
